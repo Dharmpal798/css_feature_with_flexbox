1b53f9b0f1713e915978003a4178db0479fa2023
Dharmpal798 <44724242+Dharmpal798@users.noreply.github.com>
2019-12-09 13:09:36 +0530

    css feature added

---
+++ css_feature_with_flexbox/demo.php
@@ -0,0 +1,14 @@
+<div style="right: 0px; width: 40px;" id="bussiness-enquiry">
+	<div id="enquiry" style="width: 0px; display: none; height: 300px;">
+    <div id="MyAction_OnComplete">
+          <div class="Business-enquiry" style="display: block;">
+         <h1>hiiiii everione</h1> 
+		</div>
+        </div>
+  	</div>
+
+  <div id="toppanel">
+    <div id="open-close" style="background-image: url(http://sribalajienclave.com/images/registeryouinterest.jpg);"> </div>
+  </div>
+
+    </div>
